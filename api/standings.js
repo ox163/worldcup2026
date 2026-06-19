5a84e70054d8b073b53b1078b16d519abca14d4c
abcxman <abcxman@abcxmandeMac-mini.local>
2026-06-19 19:29:40 +0800

    Add World Cup API endpoints

---
+++ api/standings.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const r = await fetch("https://api.football-data.org/v4/competitions/WC/standings", {
+    headers: {
+      "X-Auth-Token": process.env.FOOTBALL_API_KEY
+    }
+  });
+
+  const data = await r.json();
+  res.status(200).json(data);
+}
